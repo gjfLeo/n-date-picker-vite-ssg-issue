a63df8a6bbdf47e8905719b895fb3efd92715ac7
20018821 <20018821@suning.com>
2022-12-02 17:02:08 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="item">
+    <n-button>Button</n-button>
+  </div>
+  <div class="item">
+    <n-input></n-input>
+  </div>
+  <div class="item">
+    <!-- Comment next line to build successfully -->
+    <n-date-picker></n-date-picker>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { NButton, NInput, NDatePicker } from "naive-ui";
+</script>
+
+<style>
+body {
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.item {
+  max-width: 200px;
+  text-align: center;
+}
+
+.item+.item {
+  margin-top: 10px;
+}
+</style>
